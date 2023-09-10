1d193a9e22507d2bd612fd0f9ac1f8b43245d29f
Logan H-D <notquiteapex@gmail.com>
2023-09-09 20:25:08 -0400

    add no-ops for handling slow gpio

--- mcu/src/main.c
+++ mcu/src/main.c
@@ -8,10 +8,14 @@
 
 #include "usb_descriptors.h"
 
-const uint KB_COL = 16;
-const uint KB_COL_COUNT = 1;
-const uint KB_ROW = 10;
-const uint KB_ROW_COUNT = 1;
+#define _NOP() do { __asm__ __volatile__ ("nop"); } while (0)
+#define _NOPS(n) do { for(uint8_t _NOP_LOOP_I=0; _NOP_LOOP_I<n; _NOP_LOOP_I++) { _NOP(); } } while (0)
+#define KB_NOP_COUNT 18
+
+const uint KB_COL = 19;
+const uint KB_COL_COUNT = 4;
+const uint KB_ROW = 16;
+const uint KB_ROW_COUNT = 3;
 
 // https://github.com/ArmDeveloperEcosystem/st7789-library-for-pico
 const uint SCR_DIN = 2;
@@ -124,6 +128,7 @@ static void send_hid_report(uint8_t report_id) {
             uint8_t usedKeys = 0;
             for (uint8_t row=0; row<KB_ROW_COUNT; row++) {
                 gpio_put(KB_ROW + row, 1);
+                _NOPS(KB_NOP_COUNT);
                 for (uint8_t col=0; col<KB_COL_COUNT; col++) {
                     if (usedKeys >= 6) {
                         continue;
@@ -136,6 +141,7 @@ static void send_hid_report(uint8_t report_id) {
                     k++;
                 }
                 gpio_put(KB_ROW + row, 0);
+                _NOPS(KB_NOP_COUNT);
             }
 
             if (tud_suspended() && usedKeys > 0) {
