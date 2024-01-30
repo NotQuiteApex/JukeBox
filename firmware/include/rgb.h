8e4318a5a44165772a48b8e86bf58f54280389fb
Logan H-D <notquiteapex@gmail.com>
2024-01-29 22:11:21 -0500

    work on rgb stuff on firmware

--- firmware/include/rgb.h
+++ firmware/include/rgb.h
@@ -5,7 +5,13 @@
 #define JUKEBOX_WS2812_RGB_H
 
 void rgb_init(void);
-void rgb_put_pixel(uint32_t pixel_grb);
+
+uint32_t rgb_to_grbw(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
+uint32_t hsv_to_grbw(uint16_t hue, uint8_t sat, uint8_t val);
+
+void rgb_put(uint8_t idx, uint32_t pixel_grb);
+void rgb_clear(void);
+void rgb_present(void);
 void rgb_task(void);
 
 #endif // JUKEBOX_WS2812_RGB_H
